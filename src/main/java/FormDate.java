1c5e9d0a0c2a4f2b4c9431f32c7619acaa85c16c
Natalia Nevzorova <alyaryzhaya@gmail.com>
2022-09-24 23:51:12 +0300

    initial commit

---
+++ src/main/java/FormDate.java
@@ -0,0 +1,9 @@
+public class FormDate {
+
+    // для описания схемы объекта в поле birthday
+
+    public int day;
+    public int month;
+    public int year;
+
+}
---
+++ src/main/java/Post.java
@@ -0,0 +1,26 @@
+public class Post {
+
+    //  описывающий объект запроса на сервер
+
+    public FormDate birthday;
+    public String name; // поле для имени
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public Boolean subscription;
+
+//    {
+//        "birthday": {
+//        "day": 13,
+//                "month": 6,
+//                "year": 1999
+//    },
+//            "name": "Иван",
+//            "passport": "4444 № 44444444",
+//            "patronymic": "Иванович",
+//            "phone": "+7 (999)-999-99-99",
+//            "surname": "Иванов",
+//            "subscription": true
+//    }
+}
